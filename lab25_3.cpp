c473610a89a3dab68114f84cf56d6d611e6aea25
penpizza <110964198+penpizza@users.noreply.github.com>
2023-03-13 17:45:47 +0700

    completed

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-//Write function count() here.
+#include<iostream>
+#include<set>
+int count(int x[],int n) {
+    set<int> y;
+    for (int i=0; i<n; i++){
+        y.insert(x[i];)
+    }
+    return y.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
